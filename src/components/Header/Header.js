508591369bd18f01c645c078fe8dee6930b66df8
libbieiscool <whoislibbiefarrell@gmail.com>
2024-02-20 19:08:29 -0700

    basic header setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'; 
+import '../Header/Header.scss'; 
+import planet from '../../assets/icons/world.png'; 
+import heart from "../../assets/icons/heart.png"; 
+
+const Header = () => {
+  return (
+    <nav className='header'>
+        <div className='header__list--container'>
+            <ul className='header__list--ul'>
+                <li>about the dev</li>
+                <li>how to play</li>
+            </ul>
+        </div>
+        <div className='header__heartIcon'>
+            <img className='header__img' src={heart} />
+        </div>
+        <div className='header__worldIcon'>
+            <img className='header__img' src={planet}/>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Header
