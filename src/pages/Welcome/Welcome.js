b91a2a921506ab4b8374b1515f33a463e820525a
libbieiscool <whoislibbiefarrell@gmail.com>
2024-02-21 09:24:22 -0700

    adding assets and home page

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,12 +1,13 @@
 import React from 'react'; 
 import '../Welcome/Welcome.scss'; 
-
+import playButton from '../../assets/icons/playButton.png'
 
 const Welcome = () => {
   return (
     <div className='welcome'>
-      <h1>This is my welcome page</h1>
-      <h1> The blue should cover all this white space</h1>
+      <h1>xTra-terrestrial Post Nite Out Adventure</h1>
+      <img className='welcome__button' alt="Play Button" src={playButton}/>
+      {/* <h1> The blue should cover all this white space</h1> */}
     </div>
   )
 }
